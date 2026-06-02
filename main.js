46550d61e196c746db1004a70d51783ae62756bc
Kiwsety <vasumasya@gmail.com>
2026-06-02 10:00:46 -0300

    0.0.1.1js
    * fixed syntax

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function YearSelected(year):{
-	if year == 1:{
-		for (let i, i < 6, i++):{
-			print("subject-button"+i)
-			document.getElementById("subject-button"+i).innerText = "tu mami"+i
+function YearSelected(year) {
+	if (year == 1) {
+		let = 0
+		for (;i < 6;i++) {
+			print("subject-button"+i);
+			document.getElementById("subject-button"+i).innerText = "tu mami"+i;
 		}
 	}
 }
