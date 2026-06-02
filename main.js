180a3b678578d74a3269d08c56749cccb7977e02
Kiwsety <vasumasya@gmail.com>
2026-06-02 09:36:55 -0300

    0.0.5.2css + 0.0.1js
    * button size
    * container margin
    
    + YearSelected(year) function

---
+++ main.js
@@ -0,0 +1,9 @@
+function YearSelected(year):{
+	if year == 1:{
+		for (let i, i < 6, i++):{
+			print("subject-button"+i)
+			document.getElementById("subject-button"+i).innerText = "tu mami"+i
+		}
+	}
+}
+	
