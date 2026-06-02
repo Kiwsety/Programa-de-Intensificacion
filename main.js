efc68e87a2d3300c93b8a79bd48bef82b511c510
Kiwsety <vasumasya@gmail.com>
2026-06-02 13:03:45 -0300

    0.0.6html 0.0.2js
    *button transition, margin, whitespace
    *button:disabled size, margin, padding, font-size, border
    *button container display, flex-warp, content, transition
    
    + year1 and year2 subjects array
    * yearSelected renamed and modificed
    + setupButtons(subjects) function
    
    made for 0.0.5.3html

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-function YearSelected(year) {
-	if (year == 1) {
-		let = 0
-		for (;i < 6;i++) {
-			print("subject-button"+i);
-			document.getElementById("subject-button"+i).innerText = "tu mami"+i;
+const year1 = ["Matematica","Lengua","C. Naturales","C. Sociales","Ciudadania","Artistica","Ed. Fisica"]
+const year2 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia","Ciudadania","Artistica","Ed. Fisica"]
+
+let i
+
+function yearSelected(year) {
+	switch (year) {
+		case 1:
+			setupButtons(year1);
+			break;
+		
+		case 2:
+			setupButtons(year2)
+			break;
+	}
+}
+
+
+function setupButtons(subjects){
+	i = 0
+	for (;i < 8;i++) {
+		console.log("subject-button"+(i+1)+" accesed");
+		console.log(subjects.length >= i+1);
+		if (subjects.length >= i+1) {
+			document.getElementById("subject-button"+(i+1)).innerText = subjects[i];
+			document.getElementById("subject-button"+(i+1)).disabled = false;
+		}
+		else {
+			console.log("must be hidden");
+			document.getElementById("subject-button"+(i+1)).disabled = true;
 		}
 	}
 }
-	
