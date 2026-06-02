be833a49eccc964e74d639390c117d9265e9b3ce
Kiwsety <vasumasya@gmail.com>
2026-06-02 20:57:06 -0300

    0.0.4 js
    + years 41,42,43,51,52,53,61,62,63
    - years 4,5,6
    + year, orientation, subject data
    + setupContainers() to show/hide containers (NOT READY)
    * yearSelected(year) resets orientation and subject data
    * yearSelected(year) years 4,5,6 removed
    + orientationSelected(_orientation) selects the orienation
    + subjectSelected(_subject) (ready to use but not work)
    
    + 2.jpg and 3.jpg at "years\1\math"

--- main.js
+++ main.js
@@ -1,43 +1,98 @@
 const year1 = ["Matematica","Lengua","C. Naturales","C. Sociales","Ciudadania","Artistica","Ed. Fisica"];
 const year2 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia","Ciudadania","Artistica","Ed. Fisica"];
-const year3 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia","Ciudadania","Artistica","Ed. Fisica"]
-const year4 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia"]
-const year5 = ["Matematica","Lengua"]
-const year6 = ["Matematica","Lengua","Fisicoquimica"]
+const year3 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia","Ciudadania","Artistica","Ed. Fisica"];
+const year41 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia"];
+const year42 = ["Matematica","Lengua"];
+const year43 = ["Matematica","Lengua","Fisicoquimica"];
+const year51 = ["Matematica","Lengua","C. Naturales","C. Sociales","Ciudadania","Artistica","Ed. Fisica"];
+const year52 = ["Matematica"];
+const year53 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia","Ciudadania","Artistica","Ed. Fisica"];
+const year61 = ["Matematica","Lengua","Fisicoquimica","Biologia","Geografia","Historia"];
+const year62 = ["Matematica","Lengua","SIC"];
+const year63 = ["Matematica","Lengua","Fisicoquimica","GEO"];
 
 
 
 let i;
 let page = 0;
+let currentOrientation = 0;
 let maxPages = 3;
+let currentYear = 0;
+let currentSubject = 0;
 
 function yearSelected(year) {
+	currentYear = year;
+	currentOrientation = 0;
+	currentSubject = 0;
+	setupContainers();
 	switch (year) {
 		case 1:
 			setupButtons(year1);
 			break;
 		case 2:
-			setupButtons(year2)
+			setupButtons(year2);
 			break;
 		case 3:
 			setupButtons(year3);
 			break;
-		case 4:
-			setupButtons(year4)
+	}
+}
+
+function orientationSelected(_orientation) {
+	currentOrientation = _orientation;
+	currentSubject = 0;
+	setupContainers();
+	switch (_orientation) {
+		case 1:
+			switch (currentYear) {
+				case 4:
+					setupButtons(year41);
+					break;
+				case 5:
+					setupButtons(year51);
+					break;
+				case 6:
+					setupButtons(year61);
+					break;
+			}
 			break;
-		case 5:
-			setupButtons(year5);
+		case 2:
+			switch (currentYear) {
+				case 4:
+					setupButtons(year42);
+					break;
+				case 5:
+					setupButtons(year52);
+					break;
+				case 6:
+					setupButtons(year62);
+					break;
+			}
 			break;
-		case 6:
-			setupButtons(year6)
+		case 3:
+			switch (currentYear) {
+				case 4:
+					setupButtons(year43);
+					break;
+				case 5:
+					setupButtons(year53);
+					break;
+				case 6:
+					setupButtons(year63);
+					break;
+			}
 			break;
 	}
 }
 
+function subjectSeleceted(_subject){
+	currentSubject = _subject
+}
 
 function setupButtons(subjects){
 	i = 0;
 	page = 0;
+	console.log(currentYear+"°"+currentOrientation+" accesed");
 	for (;i < 8;i++) {
 		console.log("subject-button"+(i+1)+" accesed");
 		console.log(subjects.length >= i+1);
@@ -51,6 +106,41 @@ function setupButtons(subjects){
 		}
 	}
 	document.getElementById("page-number").innerText = (page+1)+"/"+maxPages;
+}
+
+function setupContainers() { //for single orientation
+	if (currentYear <= 3) {
+		if (currentSubject == 0){
+			document.getElementById("subjects").disabled = false;
+		}
+		else {
+			document.getElementById("subjects").disabled = true;
+			if (currentSubject == 0){
+				document.getElementById("image-container").disabled = false;
+			}
+			else {
+				document.getElementById("image-container").disabled = true;
+			}
+		}
+
+	}
+	else if (currentYear > 3) { //for orientable years
+		document.getElementById("orientation").disabled = false;
+		
+		if (currentOrientation == 0){
+			document.getElementById("subjects").disabled = false;
+		}
+		else {
+			document.getElementById("subjects").disabled = true;
+			
+			if (currentSubject == 0){
+				document.getElementById("image-container").disabled = true;
+			}
+			else {
+				document.getElementById("image-container").disabled = false;
+			}
+		}
+	}
 }
 
 
